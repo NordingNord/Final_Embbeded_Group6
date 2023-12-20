cf5a181d6dfbf633667935284313c516b1578bc5
Frank-JJ <frankjuuljensen1@gmail.com>
2023-12-20 20:21:54 +0100

    Conv2d now works as intended using test case.

--- CNN/src/hls/cnn.cpp
+++ CNN/src/hls/cnn.cpp
@@ -63,8 +63,11 @@ void conv2d(const int input_dims[3], float* input,
                         for (int vi = 0; vi < weight_dims[3]; vi++)
                         {
                             turns++;
-                            printf("output[%d][%d][%d], kernel[%d][%d][0][%d]\n", i-row_margin, ii-col_margin, vi, iv + row_margin, v + col_margin, vi);
-                            output[(i-row_margin)*output_dims[1]*output_dims[2] + (ii-col_margin)*output_dims[2] + iii] += input[i*input_dims[1]*input_dims[2] + ii*input_dims[2] + iii];
+                            printf("output[%d][%d][%d] = ", i-row_margin, ii-col_margin, vi);
+                            printf("input[%d][%d][%d] * ", i+iv, ii+v, iii);
+                            printf("kernel[%d][%d][0][%d]", iv + row_margin, v + col_margin, vi);
+                            printf(" = %f\n", input[(i+iv)*input_dims[1]*input_dims[2] + (ii+v)*input_dims[2] + iii]);
+                            output[(i-row_margin)*output_dims[1]*output_dims[2] + (ii-col_margin)*output_dims[2] + iii] += input[(i+iv)*input_dims[1]*input_dims[2] + (ii+v)*input_dims[2] + iii] * weights[(iv+row_margin)*weight_dims[1]*weight_dims[2]*weight_dims[3] + (v+col_margin)*weight_dims[2]*weight_dims[3] + vi];
                         }
                     }
                 }
@@ -109,14 +112,20 @@ void conv2d(const int input_dims[3], float* input,
 int main()
 {
     constexpr int input_dims[3] = {4,5,1};
-    float model_input[input_dims[0]*input_dims[1]*input_dims[2]] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16, 17, 18, 19, 20};
+    float model_input[input_dims[0]*input_dims[1]*input_dims[2]] = {5,2,1,1,5,
+                                                                    5,2,1,1,5,
+                                                                    5,2,1,1,5,
+                                                                    5,2,1,1,5,};
 
     constexpr int test_kernel_dims[4] = {3,3,1,1};
-    float test_kernel[test_kernel_dims[0]*test_kernel_dims[1]*test_kernel_dims[2]*test_kernel_dims[3]] = {1,2,3,4,5,6,7,8,9};
+    float test_kernel[test_kernel_dims[0]*test_kernel_dims[1]*test_kernel_dims[2]*test_kernel_dims[3]] = {0,1,0,
+                            0,1,0,
+                            1,1,1};
 
 
     constexpr int output_dims[3] = {2,3,1};
-    float output[output_dims[0]*output_dims[1]*output_dims[2]] = {0,0,0, 0,0,0};
+    float output[output_dims[0]*output_dims[1]*output_dims[2]] = {0,0,0, 
+                                                                0,0,0};
     
     conv2d(input_dims, model_input, 
             test_kernel_dims, test_kernel, 
